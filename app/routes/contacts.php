575339c4b1cf77ba43758b2903d4a6c0223c8109
Jacob Walker <jacob@tortugas-llc.com>
2013-08-16 13:34:44 -0500

    Ability to update contacts

--- app/routes/contacts.php
+++ app/routes/contacts.php
@@ -9,12 +9,36 @@ $app->path("contacts", function($request) use($app) {
     // further pathing because we show the contact list on every page.
     $view->set("contacts", $factory->findAll());
 
-    // Return general contact list
+    // GET /contacts : Return general contact list
     $app->get(function($request) use($app, $factory, $view) {
         return $view;
     });
 
     $app->param('int', function($request, $id) use($app, $factory, $view) {
-        return $view->set("current_contact", $factory->find($id));
+        $contact = $factory->find($id);
+        $view->set("current_contact", $contact);
+
+        // GET /contacts/n : Fetch a particular contact
+        $app->get(function($request) use($app, $view) {
+            return $view;
+        });
+
+        // POST /contacts/n : Update or create contact
+        $app->post(function($request) use($app, $view, $contact, $factory) {
+            $contact->setData("name", $request->post("input-name"));
+            $contact->setData("email", $request->post("input-email"));
+            try {
+                $contact->save();
+                $view->set("success", "Contact saved.");
+            } catch(\Exception $e) {
+                $view->set("error", "There was an error saving your contact.");
+            }
+
+            // Update the list of all contacts with our changes
+            $view->set("contacts", $factory->findAll());
+
+            return $view;
+        });
+
     });
 });
--- app/templates/contacts.html.php
+++ app/templates/contacts.html.php
@@ -2,23 +2,47 @@
 <div class="row-fluid">
     <div class="well span3" id="contact-list">
         <ul class="nav nav-list">
+            <li><a href="/contacts/0">New Contact</a></li>
             <li class="nav-header">Current Contacts</li>
             <?php foreach($contacts as $contact) { ?>
                 <?php $class = isset($current_contact) && $contact->getData("id") == $current_contact->getData("id") ? "active" : ""; ?>
-                <li class="<?php echo $class; ?>"><a href="/contacts/view/<?php echo $contact->getData("id"); ?>"><?php echo $contact->getData("name"); ?></a></li>
+                <li class="<?php echo $class; ?>"><a href="/contacts/<?php echo $contact->getData("id"); ?>"><?php echo $contact->getData("name"); ?></a></li>
             <?php } ?>
         </ul>
     </div>
     <div class="well span9">
+        <?php if (isset($success)) { ?>
+        <div class="alert alert-success">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <?php echo $success; ?>
+        </div>
+        <?php } ?>
+        <?php if (isset($error)) { ?>
+        <div class="alert alert-error">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <?php echo $error; ?>
+        </div>
+        <?php } ?>
         <?php if (isset($current_contact)) { ?>
-        <dl>
-            <dt>Name</dt>
-            <dd><?php echo $current_contact->getData("name"); ?></dd>
-            <dt>Email</dt>
-            <dd><?php echo $current_contact->getData("email"); ?></dd>
-        </dl>
+        <form method="post" class="form-horizontal">
+            <div class="control-group">
+                <label class="control-label" for="input-name">Name</label>
+                <div class="controls">
+                    <input type="text" autofocus id="input-name" name="input-name" value="<?php echo $current_contact->getData("name"); ?>" />
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="input-email">Email</label>
+                <div class="controls">
+                    <input type="email" pattern="[^ @]*@[^ @]*" id="input-email" name="input-email" value="<?php echo $current_contact->getData("email"); ?>" />
+                </div>
+            </div>
+            <div class="form-actions">
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+        </form>
         <?php } else { ?>
-            <h3>Select A Contact</h3>
+        <h3>Select A Contact</h3>
         <?php } ?>
     </div>
 </div>
